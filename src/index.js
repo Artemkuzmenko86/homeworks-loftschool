88e7eee8abfc9aa8ec59b7b7b5ad540a562f6b25
Artemkuzmenko86 <44545305+Artemkuzmenko86@users.noreply.github.com>
2018-12-07 12:55:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,122 +1,159 @@
-/* ДЗ 1 - Функции */
+/*
+ ДЗ 7 - Создать редактор cookie с возможностью фильтрации
+
+ 7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
+   - имя
+   - значение
+   - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
+
+ 7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
+   - имя
+   - значение
+   - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+
+ Если добавляется cookie с именем уже существующией cookie, то ее значение в браузере и таблице должно быть обновлено
+
+ 7.3: На странице должно быть текстовое поле для фильтрации cookie
+ В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
+ Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
+ то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
 
 /*
- Задание 1:
-
- 1.1: Добавьте к функции параметр с любым именем
- 1.2: Функция должна возвращать аргумент, переданный ей в качестве параметра
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
 
  Пример:
-   returnFirstArgument(10) вернет 10
-   returnFirstArgument('привет') вернет `привет`
-
- Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
  */
-function returnFirstArgument(a) {
-	return a
-	}
-returnFirstArgument(10);
+const homeworkContainer = document.querySelector('#homework-container');
+// текстовое поле для фильтрации cookie
+const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+// текстовое поле с именем cookie
+const addNameInput = homeworkContainer.querySelector('#add-name-input');
+// текстовое поле со значением cookie
+const addValueInput = homeworkContainer.querySelector('#add-value-input');
+// кнопка "добавить cookie"
+const addButton = homeworkContainer.querySelector('#add-button');
+// таблица со списком cookie
+const listTable = homeworkContainer.querySelector('#list-table tbody');
 
+const isMatching = (full, str) => {
+  return full.toLowerCase().indexOf(str.toLowerCase()) > -1;
+};
 
-/*
- Задание 2:
+filterNameInput.addEventListener('keyup', function() {
+  // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
 
- 2.1: Функция должна возвращать сумму переданных аргументов
+  let cookieParse = parseCookie();
 
- Пример:
-   sumWithDefaults(10, 20) вернет 30
-   sumWithDefaults(2, 4) вернет 6
+  const filterValue = filterNameInput.value;
 
- 2.1 *: Значение по умолчанию для второго аргумента должно быть равно 100
+  // Очистить таблицу при нажатии на кнопку выгрузки 
+  listTable.innerHTML = '';
 
- Пример:
-   sumWithDefaults(10) вернет 110
- */
-function sumWithDefaults(a, b = 100) {
- return a + b;
-}
-sumWithDefaults(15);
-sumWithDefaults(23);
-/*
- Задание 3:
+  // Фильтр
+  filterCookie(cookieParse, isMatching, filterValue);
 
- Функция должна принимать другую функцию и возвращать результат вызова этой функции
+});
 
- Пример:
-   returnFnResult(() => 'привет') вернет 'привет'
- */
-function returnFnResult(fn) {
-	return fn(); 
-	}	
-	// Testing 	
-	returnFnResult()('Привет');
-/*
- Задание 4:
+addButton.addEventListener('click', () => {
+  // Обработка нажатия на кнопку "добавить cookie"
 
- Функция должна принимать число и возвращать новую функцию (F)
- При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
+  // Запись в куки из формы
+  document.cookie = `${addNameInput.value}=${addValueInput.value}`;
 
- Пример:
-   var f = returnCounter(10);
+  // Очистить таблицу при нажатии на кнопку 
+  listTable.innerHTML = '';
 
-   console.log(f()); // выведет 11
-   console.log(f()); // выведет 12
-   console.log(f()); // выведет 13
- */
-function returnCounter(a = 0) {
-	return function F() {
-			return ++a;
-	}
-}
+  if(filterNameInput.value === ''){
+      // Приведение к виду объекта из строки
+      let cookie = parseCookie();
+      // Рендер 
+      renderCookie(cookie);
+  }else{
+      // Приведение к виду объекта из строки
+      let filtercookie = parseFilterCookie(filterNameInput.value);
+      // Рендер
+      renderCookie(filtercookie);
+  }
 
-// Testing 
-console.log(f());
-console.log(f());
-console.log(f());
+});
 
-/*
- Задание 5 *:
+// Функции для работы
 
- Функция должна возвращать все переданные ей аргументы в виде массива
- Количество переданных аргументов заранее неизвестно
-
- Пример:
-   returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
- */
-function returnArgumentsArray() {
-	var result = [];
-	for (var i = 0; i < arguments.length; i++) {
-			result[i] = arguments[i];
-	}
-	return result;
-}
-
-/*
- Задание 6 *:
-
- Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
- Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
-
- Пример:
-   function sum(a, b) {
-     return a + b;
-   }
-
-   var newSum = bindFunction(sum, 2, 4);
-
-   console.log(newSum()) выведет 6
- */
-function bindFunction(F, a, b) {
-	F = F.bind(null, a, b);
-	return F;
+function parseCookie() {
+  let cookie = document.cookie.split('; ').reduce((prev, current) => {
+      const [name, value] = current.split('=');
+      prev[name] = value;
+      return prev;
+  }, {});
+  return cookie;
 }
 
-export {
-    returnFirstArgument,
-    sumWithDefaults,
-    returnArgumentsArray,
-    returnFnResult,
-    returnCounter,
-    bindFunction
+function parseFilterCookie(filterValue) {
+  let cookie = document.cookie.split('; ').reduce((prev, current) => {
+      const [name, value] = current.split('=');
+      if(isMatching(name, filterValue) || isMatching(value, filterValue)){
+          prev[name] = value;
+      }
+      return prev;
+  }, {});
+  return cookie;
 }
+
+function renderCookie(cookie) {
+  for(let key in cookie){
+
+      let row = listTable.insertRow(listTable.rows.length);
+      let cell = row.insertCell(0);
+      cell.innerText = key;
+      let cell2 = row.insertCell(1);
+      cell2.innerText = cookie[key];
+      let cell3 = row.insertCell(2);
+      let button = document.createElement("BUTTON");
+      //let txt = document.createTextNode('Удалить');
+      //button.appendChild(txt);
+      button.textContent = 'Удалить';
+      cell3.appendChild(button);
+      button.addEventListener('click', function () {
+          listTable.removeChild(row);
+          document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      });
+  }
+}
+
+function filterCookie(cookieParse, isMatching, filterValue) {
+  for (let key in cookieParse) {
+      if (typeof (key) != "undefined" && typeof(cookieParse[key]) != "undefined") {
+
+          if (isMatching(key, filterValue) || isMatching(cookieParse[key], filterValue)) {
+              // add cookie to table
+
+              let row = listTable.insertRow(listTable.rows.length);
+              let cell = row.insertCell(0);
+              cell.innerText = key;
+              let cell2 = row.insertCell(1);
+              cell2.innerText = cookieParse[key];
+              let cell3 = row.insertCell(2);
+              let button = document.createElement("BUTTON");
+              button.textContent = 'Удалить';
+              cell3.appendChild(button);
+              button.addEventListener('click', function () {
+                  listTable.removeChild(row);
+                  document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+              });
+          }
+      }
+  }
+}
+
+let globalParse = parseCookie();
+
+renderCookie(globalParse);
 
